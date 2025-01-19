77fbe19d4559ecb533add63720c37d9442bcf172
Anton Prince Oommen <antonprince95@gmail.com>
2025-01-19 20:37:54 +0530

    Basic frontend

--- backend/gemini.js
+++ backend/gemini.js
@@ -1,9 +1,12 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+import dotenv from 'dotenv'
+import { GoogleGenerativeAI } from '@google/generative-ai';
+dotenv.config()
 
-const genAI = new GoogleGenerativeAI("YOUR_API_KEY");
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "Explain how AI works";
+const prompt = "These are the comments of a youtube video. I want you to analyse these comments and divide them into 3 categories - agree,disagree and neutral. Return the answer in json format, with the corresponding number of comments for each field.";
 
 const result = await model.generateContent(prompt);
 console.log(result.response.text());
