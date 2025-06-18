0ca3f4ebc6becb1cc220b506c685ddfb066a2578
antonnprince <antonprince95@gmail.com>
2025-06-18 15:58:38 +0530

    bug

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config()
 const app = express();
 
 app.use(cors({
-  origin: "https://sentiment-analysis-lac.vercel.app/"}));
+  origin: "https://sentiment-analysis-lac.vercel.app"}));
 
 app.use(express.json());
 
